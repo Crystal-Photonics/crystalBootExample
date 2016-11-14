f169b05133cd3bf8002d865580bd018b50bbf643
arne krüger <ak@crystal-photonics.com>
2016-11-14 15:44:54 +0100

    -adding project

---
+++ mcu/crystalBoot_config/key/aes_128_key.h
@@ -0,0 +1,13 @@
+/*
+ * key.h
+ *
+ *  Created on: 21.10.2016
+ *      Author: ak
+ */
+
+#ifndef MCU_BOOTLOADERCONFIG_AES_128_KEY_H_
+#define MCU_BOOTLOADERCONFIG_AES_128_KEY_H_
+#include <stdint.h>
+extern uint8_t aes_128_key[16];
+
+#endif /* MCU_BOOTLOADERCONFIG_AES_256_KEY_H_ */
